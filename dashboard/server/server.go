06790494570aed512d62534ad872e4ac9e8f1db6
Angel Pan <angelpan726@gmail.com>
2026-04-07 17:06:41 -0700

    fix closing subagents bug

--- dashboard/server/server.go
+++ dashboard/server/server.go
@@ -431,7 +431,7 @@ func (s *Server) Stop() {
 func (s *Server) corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
